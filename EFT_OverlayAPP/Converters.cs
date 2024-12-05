499a39cb4d3b993c64b94b059cbbe49078fa0990
v_kub4pl_v <jakub.filip.bury@gmail.com>
2024-12-05 11:45:09 +0100

    Fixed Boolean / Visiblity Converter

--- EFT_OverlayAPP/Converters.cs
+++ EFT_OverlayAPP/Converters.cs
@@ -215,6 +215,23 @@ namespace EFT_OverlayAPP
         }
     }
 
+    public class InverseBooleanVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class TimeSpanConverter : JsonConverter<TimeSpan>
     {
         // Serialize TimeSpan to a string
